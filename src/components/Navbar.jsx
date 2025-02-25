0c9c813b63cdd068d94d90809b5bfc1c411761e1
jemima Anyanah <northernweavegh@gmail.com>
2025-02-25 11:04:21 +0000

    update1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { FaEnvelope, FaHome, FaInfoCircle, FaProductHunt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
-    <div className="p-6 flex justify-center align-middle bg-amber-200">
-      <h4 className="text-xl font-bold">Navbar</h4>
-    </div>
+    <nav className="navbar">
+      <div className="logo">Hospitality Innovation</div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/"><FaHome /> Home</Link>
+        </li>
+        <li>
+          <Link to="/about-product"><FaProductHunt /> About Product</Link>
+        </li>
+        <li>
+          <Link to="/about-innovator"><FaUser /> About Innovator</Link>
+        </li>
+        <li>
+          <Link to="/showcase"><FaInfoCircle /> Showcase</Link>
+        </li>
+        <li>
+          <Link to="/contact"><FaEnvelope /> Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
